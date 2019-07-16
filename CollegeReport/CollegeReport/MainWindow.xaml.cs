2973159624f40c21cbfae0241cfb21d4d4505914
tjcocklin <jcocklin@email.sc.edu>
2019-07-15 21:19:39 -0400

    WPF project

---
+++ CollegeReport/CollegeReport/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CollegeReport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string DisplayFileName { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        /// <summary>
+        /// Reads in a user selected CollegeScoreCard_Raw_Data csv
+        /// file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = " csv files (MERGED*PP.csv)| merged*pp.csv";
+
+            if (dialog.ShowDialog() == true)
+            {
+                DisplayFileName = dialog.FileName;
+                SelectedFileTextBox.Text = DisplayFileName;
+
+            }
+            
+        }
+        /// <summary>
+        /// Opens a new window to process user selected csv file.
+         /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void FileOpenButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            
+            SearchWindow searchWindow = new SearchWindow(DisplayFileName);
+            searchWindow.Show();
+            this.Close();
+            
+            
+        }
+    }
+}
---
+++ CollegeReport/README.md
@@ -0,0 +1,4 @@
+A project that demonstrates the use of file IO using WPF.
+Reads .csv files provided by CollegeScoreCard under the foldername
+CollegeScoreCard_Raw_Data. Data Files are Available for download 
+here: https://collegescorecard.ed.gov/data/
